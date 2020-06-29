4003e5a327ac15f9ccf02c8a60db7b485475cc0f
Michel dos Santos <michelsantos15@gmail.com>
2020-06-28 22:53:21 -0300

    começando a implementar a lógica do jogo

--- app/src/main/java/micheldev/com/br/MainActivity.kt
+++ app/src/main/java/micheldev/com/br/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    val playerOne = arrayListOf<Int>()
+    val playertwo = arrayListOf<Int>()
+    var currentplayer = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
